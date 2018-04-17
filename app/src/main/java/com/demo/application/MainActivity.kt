aea1da054b34eed4475e69bd9646eca535b46b84
devlopmentuser123 <developmentuser123@gmail.com>
2018-04-17 19:00:27 +0530

    Test ARCore
    initial ARCore App

---
+++ app/src/main/java/com/demo/application/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.demo.application
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //added changes 17 april
+    }
+}
